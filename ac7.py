c2f94d8c4768b1e933bf832e8c8b9d31dc6abe57
affan50pro <sweet.baby12325@gmail.com>
2024-10-16 20:27:16 +0400

    first commit

---
+++ ac7.py
@@ -0,0 +1,8 @@
+math=int(input("math=enter your percentage"))
+science=int(input("science=enter your percentage"))
+english=int(input("english=enter your percentage"))
+Urdu=int(input("urdu=enter your percentage"))
+num=math+science+english+Urdu
+print("your total percentage is=",num)
+num2=num/4
+print("your overall percentage is",num2)
---
+++ ac8.py
@@ -0,0 +1,7 @@
+print(10==10)
+print(10==9)
+print(10!=9)
+print(6<9)
+print(9<6)
+print(10<=10)
+print(7>=6)
